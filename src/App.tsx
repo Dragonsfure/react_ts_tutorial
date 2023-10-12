56a97ba30c10d77c6c53cf81b6d74cfd02c6b005
AnosVoldigoad02 <95828255+AnosVoldigoad02@users.noreply.github.com>
2023-10-12 17:35:15 +0200

    First real commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  	const title : string= "Welcome to test blog";
+    const likes: number = 50 ; 
+  	const link: string = "http://www.google.com";
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className='content'>
+      <h1>{ title }</h1>
+      <p>Liked {likes} times </p>
+
+
+      <p>{10}</p>
+      <p>{"hello, ninjas"}</p>
+      <p>{[1,2,3,4,5]}</p>
+      <p>{Math.random() * 10}</p>
+
+      <a href={link}> Google Sites</a>
+     </div>
     </div>
   );
 }
