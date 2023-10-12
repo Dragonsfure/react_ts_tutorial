c90ca8164cde0b4f3c7ef3987b81b715cf5ddff8
AnosVoldigoad02 <95828255+AnosVoldigoad02@users.noreply.github.com>
2023-10-12 18:49:02 +0200

    Improved the update syntax

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,24 +1,21 @@
-import { click } from "@testing-library/user-event/dist/click";
 import React, { MouseEvent, useState } from "react";
 
 interface IAppProps {}
 
 const Home: React.FunctionComponent<IAppProps> = (props) => {
-  const [clickVal, updateval] = useState({ value:  0});
-
+    //gets an array of the instance and a function to change the instance 
+  let [clickVal, updateValue] = useState(0);
   const OnClick = (e: MouseEvent<HTMLButtonElement>) => {
     console.log("clicked");
-    updateval({
-      value: (++(clickVal.value as number)),
-    });
-    console.log( clickVal.value);
+    updateValue(++clickVal);
+    console.log( clickVal);
   };
 
   return (
     <div className="Home">
       <h2>Homepage</h2>
       <button onClick={OnClick}>Click me</button>
-      <p>Button clicked: {clickVal.value} </p>
+      <p>Button clicked: {clickVal} </p>
     </div>
   );
 };
