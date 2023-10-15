4737ecb3cc25dde4ed708d96918e1492c8a35a21
Dragonsfure <ae_a18@outlook.de>
2023-10-15 13:10:46 +0200

    Using Fetch with a local Json-Server To show a usage with an "api" Also type saving the List

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import Navbar from "./Navbar";
 import Home from "./Home";
 import IBlog from "./interfaces/IBlog";
 
-
 function App() {
   const title: string = "Welcome to test blog";
   const likes: number = 50;
   const link: string = "http://www.google.com";
-  const [blogs,setblogs ] = useState<IBlog[]>([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    { title: "Web dev top tips", body: "lorem ipsum...", author: "mario", id: 3 },
-  ]);
+  const [blogs, setblogs] = useState<IBlog[] | null>(null);
 
-  const handleDelete=(id:number) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id)
-    setblogs(newBlogs);
-  }
+  const [name, setName] = useState<string>("mario");
+
+  const handleDelete = (id: number) => {
+    const newBlogs = blogs?.filter((blog) => blog.id !== id);
+    if (newBlogs !== undefined) {
+      setblogs(newBlogs);
+    }
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json() as Promise<IBlog[]>;
+      })
+      .then((data) => {
+        console.log(data);
+        setblogs(data);
+      });
+  }, []);
 
   return (
     <div className="App">
       <Navbar />
 
-
       <div className="content">
-        <Home
-          blogs={blogs}
-          title="All Blogs"
-          handleDelete = {handleDelete}
-        />
+        <Home blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+
+        <button onClick={() => setName("luigi")}>change name</button>
+        <p>{name}</p>
       </div>
     </div>
   );
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,23 +1,22 @@
 import React, { MouseEvent, useEffect, useState } from "react";
 import IBlog from "./interfaces/IBlog";
 
-interface BlogsProps{
-    title: string,
-    blogs: IBlog[];
-    handleDelete: Function
+interface BlogsProps {
+  title: string;
+  blogs: IBlog[] | null;
+  handleDelete: Function;
 }
 
-const Home: React.FunctionComponent<BlogsProps> = (props:BlogsProps) => {
- 
+const Home: React.FunctionComponent<BlogsProps> = (props: BlogsProps) => {
   return (
     <div className="Home">
-        <h1>{props.title}</h1>
-      {props.blogs.map(item  => (
-        <div className="blog-preview" key={item.id} >
-        <h2>{ item.title }</h2>
-        <p>Written by { item.author }</p>
-        <button onClick={()=> props.handleDelete(item.id)}>Delete</button>
-      </div>
+      <h1>{props.title}</h1>
+      {props.blogs?.map((item) => (
+        <div className="blog-preview" key={item.id}>
+          <h2>{item.title}</h2>
+          <p>Written by {item.author}</p>
+          <button onClick={() => props.handleDelete(item.id)}>Delete</button>
+        </div>
       ))}
     </div>
   );
