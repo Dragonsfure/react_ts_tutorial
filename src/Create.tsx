eb16cd8359a2c7a289efe0a56a452d6467918b0f
Dragonsfure <ae_a18@outlook.de>
2023-10-16 22:22:13 +0200

    expanding the "database" with create form
    Also adding more css for the form

--- src/Create.tsx
+++ src/Create.tsx
@@ -1,12 +1,64 @@
-import * as React from 'react';
+import * as React from "react";
+import { useState } from "react";
+import IBlog from "./interfaces/IBlog";
 
-interface ICreateProps {
-}
+interface ICreateProps {}
+
+type Blogg = {
+  title: string;
+  body: string;
+  author: string;
+};
 
 const Create: React.FunctionComponent<ICreateProps> = (props) => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("");
+  const [isPending, setIsPending] = useState(false)
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const blog: Blogg = { title, body, author };
+
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("new blog added");
+      setIsPending(false);
+    });
+  };
+
   return (
-    <div>
-        <h1>Add a new Blog</h1>
+    <div className="create">
+      <h1>Add a new Blog</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Blog Title: </label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <label>Blog Body: </label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+
+        <label>Blog Author: </label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+
+       { !isPending && <button >Create Blog</button> }
+       { isPending && <button disabled >Adding Blog...</button> }
+
+      </form>
     </div>
   );
 };
