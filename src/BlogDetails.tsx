3d68463269cb1638d2f621a9a3623410d0c9ef50
Dragonsfure <ae_a18@outlook.de>
2023-10-16 19:21:32 +0200

    Blog Details and More css

--- src/BlogDetails.tsx
+++ src/BlogDetails.tsx
@@ -1,16 +1,33 @@
-import * as React from 'react';
+import * as React from "react";
 import { useParams } from "react-router-dom";
 import IParam from "./interfaces/IParam";
+import useFetch from "./useFetch";
+import IBlog from "./interfaces/IBlog";
 
-interface IBlogDetailsProps {
-
-}
+interface IBlogDetailsProps {}
 
 const BlogDetails: React.FunctionComponent<IBlogDetailsProps> = (props) => {
   const params = useParams<IParam>();
+  const hookData = useFetch("http://localhost:8000/blogs/" + params.id);
+  let newData: IBlog;
+  if (hookData.data !== null && (hookData.data as unknown as IBlog)) {
+    newData = hookData.data as unknown as IBlog;
+    return (
+      <div className="blog-details">
+        {hookData.isPending && <div>Loading ...</div>}
+        {hookData.error && <div>{hookData.error.message}</div>}
+        <article>
+          <h2>{newData.title}</h2>
+          <p>Written by {newData.author}</p>
+          <div>{newData.body}</div>
+        </article>
+      </div>
+    );
+  }
   return (
     <div className="blog-details">
-      <h1>Blog Details - {params.id} </h1>
+      {hookData.isPending && <div>Loading ...</div>}
+      {hookData.error && <div>{hookData.error.message}</div>}
     </div>
   );
 };
