34ac1747655e4d59d6d33bba4f397ba60bfe08d9
Dragonsfure <ae_a18@outlook.de>
2023-10-17 20:45:33 +0200

    Deleting of Block
    Added Deleting  and Svg to Delete Button

--- src/BlogDetails.tsx
+++ src/BlogDetails.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import IParam from "./interfaces/IParam";
 import useFetch from "./useFetch";
 import IBlog from "./interfaces/IBlog";
@@ -10,6 +10,16 @@ const BlogDetails: React.FunctionComponent<IBlogDetailsProps> = (props) => {
   const params = useParams<IParam>();
   const hookData = useFetch("http://localhost:8000/blogs/" + params.id);
   let newData: IBlog;
+  const history = useHistory();
+
+  const handleClick = () => {
+    fetch("http://localhost:8000/blogs/" + newData.id, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+    });
+  };
+
   if (hookData.data !== null && (hookData.data as unknown as IBlog)) {
     newData = hookData.data as unknown as IBlog;
     return (
@@ -20,6 +30,9 @@ const BlogDetails: React.FunctionComponent<IBlogDetailsProps> = (props) => {
           <h2>{newData.title}</h2>
           <p>Written by {newData.author}</p>
           <div>{newData.body}</div>
+          <button className="blog-details del button" onClick={handleClick}>
+            <img src="/trashcan.svg" alt="delete icon"></img>
+          </button>
         </article>
       </div>
     );
